7b5e06121187cef6a23dc4b4c54d37cd5024d025
Kitt Tientanopajai <kitt@kku.ac.th>
2022-07-29 17:47:19 +0700

    Add static-nodes.json volume mapping

--- start-node.sh
+++ start-node.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 docker run -dit --restart=unless-stopped \
-	--name testnet-xx \
-	-v $PWD:/srv -w /srv \
+	--name mainnet-xxx \
+	-v $PWD:/srv \
+	-v $PWD/static-nodes.json:/srv/node/geth/static-nodes.json:ro \
+	-w /srv \
 	-p 30303:30303 \
 	ethereum/client-go:stable \
 	--networkid 1707 \
 	--port 30303 \
 	--datadir /srv/node \
-	--unlock 1234567890abcdef1234567890abcdef12345678 \
+	--unlock 0x1234567890abcdef1234567890abcdef12345678 \
 	--password password.txt \
 	--gcmode archive \
 	--syncmode full \
